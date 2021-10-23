b9df1f67bb656162403e4740bfba427b958f5e43
Anushan Santhirakumar <73377435+Anushan1508@users.noreply.github.com>
2021-10-23 14:10:28 +0530

    Create for.py

---
+++ Python/Loops/for.py
@@ -0,0 +1,10 @@
+#A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
+#This is less like the for keyword in other programming languages, and works more like an iterator method as found in other object-orientated programming languages.
+#With the for loop we can execute a set of statements, once for each item in a list, tuple, set etc.
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+  
+ 
+#The for loop does not require an indexing variable to set beforehand.
